0b9c111794aecca2add0f5d152cc8cc1b7b6cc10
tar04 <taras200195@gmail.com>
2021-11-16 22:58:42 +0200

    added less3

--- lesson3/classwork/clw_3.js
+++ lesson3/classwork/clw_3.js
@@ -1,6 +1,6 @@
 //зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
 
-let arr=[2,17,13,6,22,31,45,66,100,-18];
+//let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
 
 //1. перебрати його циклом while
 
@@ -38,7 +38,7 @@ let arr=[2,17,13,6,22,31,45,66,100,-18];
 
 //let i=0;
 //while (i<arr.length){
-//    if(i%2===0){
+//    if(arr[i]%2===0){
 //        console.log(arr[i]);
 //    }
 //    i++;
@@ -47,7 +47,7 @@ let arr=[2,17,13,6,22,31,45,66,100,-18];
 //6. перебрати циклом for та вивести  числа тільки парні  значення
 
 //for (let i = 0; i < arr.length; i++) {
-//    if (i%2===0){
+//    if (arr[i]%2===0){
 //        console.log(arr[i]);
 //    }
 //}
@@ -82,11 +82,50 @@ let arr=[2,17,13,6,22,31,45,66,100,-18];
 //}
 
 //3)
+//let i=arr.length-1;
+//while(i>=0) {
+//    if (i%2!==0){
+//        console.log(arr[i]);
+//    }
+//    i--;
+//}
 
-let i=arr.length-1;
-while(i>=0) {
-    if (i%2!==0){
-        console.log(arr[i]);
-    }
-    i--;
-}
+//4)
+//for (let i = arr.length - 1; i >= 0; i--) {
+//    if (i%2!==0){
+//        console.log(arr[i]);
+//    }
+//
+//}
+
+//5)
+//let i=arr.length;
+//while (i>=0){
+//    if(arr[i]%2===0){
+//        console.log(arr[i]);
+//    }
+//    i--;
+//}
+
+//6)
+//for (let i = arr.length - 1; i >= 0; i--) {
+//    if (arr[i] % 2 === 0) {
+//        console.log(arr[i]);
+//    }
+//
+//}
+
+//6)
+//for (let i = arr.length - 1; i >= 0; i--) {
+//    if (arr[i] % 2 === 0) {
+//        console.log(arr[i]);
+//    }
+//}
+
+//7)
+//for (let i = arr.length - 1; i >= 0; i--) {
+//    if (arr[i] % 3 === 0) {
+//        arr[i] = 'okten';
+//    }
+//    console.log(arr[i]);
+//}
