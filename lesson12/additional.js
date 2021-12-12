14b841d9064229c06e06581ddbd4fe39db81b7ee
tar04 <taras200195@gmail.com>
2021-12-12 03:57:41 +0200

    added lesson 12

--- lesson12/additional.js
+++ lesson12/additional.js
@@ -24,35 +24,61 @@ fetch('https://jsonplaceholder.typicode.com/users')
                             userDiv.innerText += `\n`;
                         }
                     }
-
                 }
             }
             userDiv.innerText += `\n`;
-            let btn1=document.createElement('button');
+            let btn1 = document.createElement('button');
             btn1.innerText = 'Load posts';
-
+            btn1.style.margin = '10px 0';
+            userDiv.append(btn1);
+            userDiv.style.borderBottom = '2px solid red';
             btn1.onclick = function () {
                 fetch('https://jsonplaceholder.typicode.com/posts')
                     .then(response => response.json())
                     .then(posts => {
                         for (const post of posts) {
-                            if (user.id === posts.userId) {
+                            if (user.id === post.userId) {
                                 let postDiv = document.createElement('div');
-                                let postId=document.createElement('h2');
-                                postId.innerText=`Id : ${post.id}`;
-                                let title=document.createElement('h1');
-                                title.innerText=post.title;
-                                let body=document.createElement('p');
-                                body.innerText=post.body;
-                                postDiv.append(postId,title,body);
+                                postDiv.setAttribute('class', 'post');
+                                let postId = document.createElement('h6');
+                                postId.innerText = `Id : ${post.id}`;
+                                let title = document.createElement('h3');
+                                title.innerText = post.title;
+                                let body = document.createElement('p');
+                                body.innerText = post.body;
+                                let btn2 = document.createElement('button');
+                                btn2.innerText = 'Load comments';
+                                btn2.style.marginTop = '8px';
+                                btn2.onclick = function () {
+                                    console.log(2)
+                                    fetch('https://jsonplaceholder.typicode.com/comments')
+                                        .then(response => response.json())
+                                        .then(comments => {
+                                            for (const comment of comments) {
+                                                if (post.id === comment.postId) {
+                                                    let commentDiv = document.createElement('div');
+                                                    commentDiv.setAttribute('class', 'comment');
+                                                    let commId = document.createElement('h4');
+                                                    commId.innerText = `Comment id: ${comment.id}`;
+                                                    let commName = document.createElement('h3');
+                                                    commName.innerText = `Name: ${comment.name}`;
+                                                    let commEmail = document.createElement('h5');
+                                                    commEmail.innerText = `Email: ${comment.email}`;
+                                                    let commBody = document.createElement('p');
+                                                    commBody.innerHTML = comment.body;
+                                                    commentDiv.append(commId, commName, commEmail, commBody);
+                                                    postDiv.appendChild(commentDiv);
+                                                }
+                                            }
+                                        });
+                                    this.disabled = true;
+                                }
+                                postDiv.append(postId, title, body, btn2);
                                 userDiv.appendChild(postDiv);
                             }
                         }
                     });
                 this.disabled = true;
             }
-
-            userDiv.appendChild(btn1);
-            userDiv.innerHTML += `<hr>`;
         }
     });
