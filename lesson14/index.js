4092ed413322c980339a7d6244cdae2b8d25b367
tar04 <taras200195@gmail.com>
2021-12-19 03:30:34 +0200

    added lesson 14

--- lesson14/index.js
+++ lesson14/index.js
@@ -5,110 +5,280 @@
 // // Наприклад. // Прокинутись - 0.3с // Поснідати - 1с // Піти в душ - 0.5с // Дочекатись автобус - 3с // Пообідати - 1с // // І так далі
 
 //Callbacks
+//function wakeUp(wokeUp, cb) {
+//    setTimeout(() => {
+//        if (wokeUp) {
+//            console.log('Прокинутись');
+//            cb();
+//        } else {
+//            console.log('Давай вставай уже');
+//            cb();
+//        }
+//    }, 3000);
+//}
+//
+//function breakfast() {
+//    setTimeout(() => {
+//        console.log('Сніданок');
+//        wash();
+//    }, 1000);
+//}
+//
+//function wash() {
+//    setTimeout(() => {
+//        console.log('Прийняти душ');
+//        workPlace();
+//    }, 2000);
+//}
+//
+//function workPlace() {
+//    setTimeout(() => {
+//            console.log('Поїхати в офіс');
+//            work();
+//    }, 500);
+//}
+//
+//let i=true;
+//
+//function work() {
+//    setTimeout(() => {
+//        console.log('Працюємо!!');
+//        if (i){
+//            dinner(true);
+//        }else{
+//            goHome()
+//        }
+//    }, 3000);
+//}
+//
+//function dinner(wantToEat) {
+//    setTimeout(() => {
+//        if (wantToEat) {
+//            console.log('Обід');
+//            work();
+//            i=false;
+//        } else {
+//            work();
+//            i=false;
+//        }
+//    }, 300);
+//}
+//
+//function goHome() {
+//    setTimeout(()=>{
+//        console.log('Поїхати додому');
+//        training();
+//    },500)
+//}
+//
+//function training() {
+//    setTimeout(() => {
+//        console.log('Тренування');
+//        supper();
+//    }, 1000);
+//}
+//
+//function supper() {
+//    setTimeout(() => {
+//        console.log('Вечеря');
+//        freeTime();
+//    }, 300);
+//}
+//
+//function freeTime() {
+//    setTimeout(() => {
+//        console.log('Прочитати книгу(мінімум 40 сторінок)');
+//        brushTeeth();
+//    }, 1000);
+//}
+//
+//function brushTeeth() {
+//    setTimeout(() => {
+//        console.log('Почистити зуби');
+//        sleep()
+//    }, 200);
+//}
+//
+//function sleep() {
+//    setTimeout(() => {
+//        console.log('Лягаємо спати');
+//    }, 100);
+//}
+//
+//wakeUp(true, breakfast);
 
-function wakeUp(wokeUp, cb) {
-    setTimeout(() => {
-        if (wokeUp) {
-            console.log('Прокинутись');
-            cb();
-        } else {
-            console.log('Давай вставай уже');
-            cb();
-        }
-    }, 3000);
+//new Promise
+function wakeUp(wokeUp) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (wokeUp) {
+                resolve('Прокинутись')
+            } else {
+                reject('Давай вставай уже');
+            }
+        }, 3000);
+    })
+
 }
 
 function breakfast() {
-    setTimeout(() => {
-        console.log('Сніданок');
-        wash();
-    }, 1000);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Сніданок');
+        }, 1000);
+    })
+
 }
 
 function wash() {
-    setTimeout(() => {
-        console.log('Прийняти душ');
-        workPlace();
-    }, 2000);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Прийняти душ');
+        }, 2000);
+    })
+
 }
 
 function workPlace() {
-    setTimeout(() => {
-            console.log('Поїхати в офіс');
-            work();
-    }, 500);
-}
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Поїхати в офіс');
+        }, 500);
+    })
 
-let i=true;
+}
 
 function work() {
-    setTimeout(() => {
-        console.log('Працюємо!!');
-        if (i){
-            dinner(true);
-        }else{
-            goHome()
-        }
-    }, 3000);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Працюємо!!');
+        }, 3000);
+    })
 }
 
+
 function dinner(wantToEat) {
-    setTimeout(() => {
-        if (wantToEat) {
-            console.log('Обід');
-            work();
-            i=false;
-        } else {
-            work();
-            i=false;
-        }
-    }, 300);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (wantToEat) {
+                resolve('Обід');
+            } else {
+                reject('Легкий перекус');
+            }
+
+        }, 300);
+    })
 }
 
 function goHome() {
-    setTimeout(()=>{
-        console.log('Поїхати додому');
-        training();
-    },500)
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Поїхати додому');
+        }, 500)
+    })
 }
 
 function training() {
-    setTimeout(() => {
-        console.log('Тренування');
-        supper();
-    }, 1000);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Тренування');
+        }, 1000);
+    })
 }
 
 function supper() {
-    setTimeout(() => {
-        console.log('Вечеря');
-        freeTime();
-    }, 300);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Вечеря');
+        }, 300);
+    })
 }
 
 function freeTime() {
-    setTimeout(() => {
-        console.log('Прочитати книгу(мінімум 40 сторінок)');
-        brushTeeth();
-    }, 1000);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Прочитати книгу(мінімум 40 сторінок)');
+        }, 1000);
+    })
 }
 
 function brushTeeth() {
-    setTimeout(() => {
-        console.log('Почистити зуби');
-        sleep()
-    }, 200);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Почистити зуби');
+        }, 200);
+    })
+
 }
 
 function sleep() {
-    setTimeout(() => {
-        console.log('Лягаємо спати');
-    }, 100);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Лягаємо спати');
+        }, 100);
+    })
+
 }
 
-wakeUp(true, breakfast);
+wakeUp(true)
+    .then(morning => {
+        console.log(morning);
 
-//new Promise
+        return breakfast();
+    })
+    .then(bf => {
+        console.log(bf);
+
+        return wash();
+    })
+    .then(washing => {
+        console.log(washing);
+
+        return workPlace();
+    })
+    .then(wp => {
+        console.log(wp);
+
+        return work()
+    })
+    .then(work => {
+        console.log(work);
+
+        return dinner(false);
+    })
+    .then(eating => {
+        console.log(eating);
+
+        return goHome();
+    })
+    .then(home => {
+        console.log(home);
+
+        return training();
+    })
+    .then(train => {
+        console.log(train);
+
+        return supper();
+    })
+    .then(supper => {
+        console.log(supper);
+
+        return freeTime();
+    })
+    .then(reading => {
+        console.log(reading);
+
+        return brushTeeth();
+    })
+    .then(brushTeeth => {
+        console.log(brushTeeth);
+
+        return sleep();
+    })
+    .then(sleep => {
+        console.log(sleep);
+    })
 
 
 //async/await
