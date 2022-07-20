2c0579f096dc5e1149b59bb7454091049c0a7843
tar04 <taras200195@gmail.com>
2022-07-21 01:20:37 +0300

    less9

--- new/lesson8/classwork.js
+++ new/lesson8/classwork.js
@@ -3,39 +3,65 @@
 //1) Напишіть код, який :
 
 //a) змінює клас тексту елементу з ід main_header на назву групи в якій ви вчитесь (mon-year)
+document.getElementById('main_header').innerText='dec-2021'
 
-
-//b) робить шириниу елементу ul 400px
+//b) робить ширину елементу ul 400px
+for (let el of document.getElementsByTagName('ul')) {
+    el.style.width='400px'
+}
 
 //c) робить ширину всіх елементів з класом linkList шириною 50%
-
+for (let el of document.getElementsByClassName('linkList')) {
+    el.style.width='50%'
+}
 
 //d) отримує текст який зберігається в елементі з класом listElement2
-
+console.log(document.getElementsByClassName('listElement2')[0].innerText);
 
 //e) отримує всі елементи li та змінює ім колір фону на сірий
-
+for (let el of document.getElementsByTagName('li')) {
+    el.style.background='gray'
+}
 
 //f) отримує всі елементи 'a' та додає їм клас anchor
-
+for (let el of document.getElementsByTagName('a')) {
+    el.classList.add('anchor')
+}
 
 //g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
-
+for (let el of document.getElementsByTagName('a')) {
+    if (el.innerText==='link3') el.style.fontSize='40px';
+}
 
 //h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
-
+for (let el of document.getElementsByTagName('a')) {
+    el.classList.add(`element_${el.innerText}`)
+}
 
 //i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
-
+// let color=prompt('Введи колір ')
+// for (let el of document.getElementsByClassName('sub-header')) {
+//     el.style.background=color
+// }
 
 //j) отримує всі елементи 'sub-header' та змінює колір тексту у випадку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
-
+// let color=prompt('Введи колір ');
+// for (let el of document.getElementsByClassName('sub-header')) {
+//     if (el.innerText==='content 2 segment'){
+//         el.style.color=color
+//     }
+// }
 
 //k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
-
+// document.getElementsByClassName('content_1')[0].innerText=prompt('Введіть текст')
 
 //l) отримати елементи p та змінити їм padding на 20px
-
+for (let el of document.getElementsByTagName('p')) {
+    el.style.padding='20px'
+}
 
 //m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year. Пример sep-2021)
 
+for (let el of document.getElementsByClassName('text2')) {
+    el.innerText='sep-2021'
+}
--- new/lesson8/homework.js
+++ new/lesson8/homework.js
@@ -2,25 +2,31 @@
 
 //- Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
 //-- отримує текст з параграфа з id "content"
-
+// console.log(document.getElementById('content').innerText);
 
 //-- отримує текст з блоку з id "rules"
-
+// console.log(document.getElementById('rules').innerText);
 
 //-- замініть текст параграфа з id 'content' на будь-який інший
-
+// document.getElementById('content').innerText='qqqq'
 
 //-- замініть текст параграфа з id 'rules' на будь-який інший
-
+// document.getElementById('rules').innerHTML='<i>hello</i>'
 
 //-- змініть кожному елементу колір фону на червоний
-
+for (let child of document.body.children) {
+    child.style.background='red'
+}
 
 //-- змініть кожному елементу колір тексту на синій
-
+for (let child of document.body.children) {
+    child.style.color='blue'
+}
 
 //-- отримати весь список класів елемента з id=rules і вивести їх в console.log
-
+console.log(document.getElementById('rules').classList)
 
 //-- поміняти колір тексту у всіх елементів fc_rules на червоний
-
+for (let el of document.getElementsByClassName('fc_rules')) {
+    el.style.color='red'
+}
