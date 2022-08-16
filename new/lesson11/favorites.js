36499720f37cb9258a7d59e149b187e074e4c51c
tar04 <taras200195@gmail.com>
2022-08-16 23:47:17 +0300

    added object methods description

--- new/lesson11/favorites.js
+++ new/lesson11/favorites.js
@@ -2,9 +2,12 @@
 const users = JSON.parse(localStorage.getItem('users'));
 
 for (const user of users) {
+
     const main = document.createElement('div');
     const h2 = document.createElement('h2');
+
     h2.innerText=JSON.stringify(user)
+
     main.append(h2)
     document.body.appendChild(main)
 }
